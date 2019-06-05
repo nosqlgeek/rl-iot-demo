90886fdff3cf00e188f78087c8e3e4517e7062ea
David Maier <david@nosqlgeeks.de>
2019-06-05 16:02:13 +0200

    Update test_zway.bash

--- setup/test_zway.bash
+++ setup/test_zway.bash
@@ -1,3 +1,4 @@
 # You need to create a user 'polly' with password 'redisredis' first!
+## You can also use i.e. ?since=1559743182000 to limit the result set
 export ZUSER=polly:redisredis
 curl -u $ZUSER http://localhost:8083/ZAutomation/api/v1/notifications | json_pp
